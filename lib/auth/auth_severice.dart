f13cba742796e3e89de32b7c2ee07f3bc3b3859d
Naresh0298 <nareshmahendhar19@gmail.com>
2024-12-28 14:58:55 +0000

    Competeled auth setup using firebase

---
+++ lib/auth/auth_severice.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthSeverice {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//sigin
+  Future<UserCredential> signInWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  //sigin up
+  Future<UserCredential> signUpWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  //sigin out
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
+
+  //errors
+}
